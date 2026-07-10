a962fc120b6e15c0418bda6f4a722ffaaa07bd0a
Bipasa Roy <bipasaroy453@gmail.com>
2026-07-10 17:43:59 +0530

    feat: implement authentication and role-based authorization middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,1 +1,13 @@
 import express from "express";
+import { createUser, loginUser, getUserProfile } from "../controllers/user.controller.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
+import { authorizeRoles } from "../middlewares/role.middleware.js";
+
+
+const router = express.Router();
+
+router.post("/register", createUser);
+router.post("/login", loginUser);
+router.get("/profile", authenticate, getUserProfile);
+
+export default router;
