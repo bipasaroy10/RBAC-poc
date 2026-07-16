11b73f0df424c97ad03c0a700d8c43caaf50d103
Bipasa Roy <bipasaroy453@gmail.com>
2026-07-16 14:03:40 +0530

    update controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -19,8 +19,11 @@ export const createUser = async (req, res) => {
             role
         });
 
-       
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        user.password = hashedPassword;
         await user.save();
+
         res.status(201).json({ message: "User created successfully", user });
     } catch (error) {
         res.status(400).json({ message: error.message });
@@ -33,15 +36,17 @@ export const loginUser = async (req, res) => {
         const { email, password } = req.body;
         const user = await User.findOne({ email });
 
-        if (!user || user.password !== password) {
+        if (!user) {
             return res.status(401).json({ message: "Invalid email or password" });
         }
 
-        if (password) {
-            user.password = await bcrypt.hash(password, 10);
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            });
         }
-
-
         const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
         res.status(200).json({ message: "Login successful", user, token });
 
