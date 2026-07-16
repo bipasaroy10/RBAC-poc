17a20d4d7131ed90e4f9206543483c21bf7e433d
Bipasa Roy <bipasaroy453@gmail.com>
2026-07-16 13:42:37 +0530

    update controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,6 +18,8 @@ export const createUser = async (req, res) => {
             password,
             role
         });
+
+       
         await user.save();
         res.status(201).json({ message: "User created successfully", user });
     } catch (error) {
@@ -34,7 +36,12 @@ export const loginUser = async (req, res) => {
         if (!user || user.password !== password) {
             return res.status(401).json({ message: "Invalid email or password" });
         }
-        
+
+        if (password) {
+            user.password = await bcrypt.hash(password, 10);
+        }
+
+
         const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
         res.status(200).json({ message: "Login successful", user, token });
 
